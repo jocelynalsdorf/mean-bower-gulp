ee192122d415f388f67d74f87e1271e4cbf7bc84
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-28 15:14:16 -0700

    add nodemon and watch package setup

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,11 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
+var nodemon = require('gulp-nodemon');
 
 //define a task called cass
 gulp.task('css', function(){
-  //grab the less file, process the less, and save to style.css
+  //grab the less file, process the less, and save to style.min.css
   return gulp.src('public/assets/css/style.less')
   .pipe(less())
   .pipe(minifyCSS())
@@ -46,6 +47,38 @@ gulp.task('angular', function(){
     .pipe(gulp.dest('public/dist'))
 
 });
+
+
+gulp.task('watch', function(){
+  //watch less file and run css stuff on changes
+  gulp.watch('public/assets/css/style.less', ['css']);
+  //watch js files and run lint and run js and angular tasks
+  gulp.watch(['server.js', 'public/app/*.js', 'public/app/**/*.js'], ['js', 'angular']);
+});
+
+gulp.task('nodemon', function(){
+  nodemon({
+    script: 'server.js',
+    ext: 'js less html'
+  })
+    .on('start', ['watch'])
+    .on('change', ['watch'])
+    .on('restart', function(){
+      console.log('Restarted');
+    });
+});
+//define the main gulp task
+gulp.task('default', ['nodemon']);
+
+
+
+
+
+
+
+
+
+
 
 
 
