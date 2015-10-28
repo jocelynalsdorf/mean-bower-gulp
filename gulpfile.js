91c96fe19cc2ac13fbde9283dd2a2f20efea2b09
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-28 14:34:14 -0700

    add minification and gulp-rename to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 //define a task called cass
 gulp.task('css', function(){
   //grab the less file, process the less, and save to style.css
   return gulp.src('public/assets/css/style.less')
   .pipe(less())
+  .pipe(minifyCSS())
+  .pipe(rename({suffix: '.min'}))
   .pipe(gulp.dest('public/assets/css'));
 });
